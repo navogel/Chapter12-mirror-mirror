a9f845211ddf6a43dd5d9c2c15d9b3f0459f6bfb
Nate V <navogel@gmail.com>
2019-09-03 17:37:01 -0500

    solved scrolling and scaling

--- js/main.js
+++ js/main.js
@@ -1,9 +1,33 @@
 console.log("hello");
 
-let outputEl1 = document.querySelector("#unique1");
-let outputEl2 = document.querySelector("#unique2");
+const audrey = document.getElementById("audrey");
+let scroll = window.scrollY;
 
-document.querySelector("#message").addEventListener("keyup", event => {
-	outputEl1.innerHTML = event.target.value;
-	outputEl2.innerHTML = event.target.value;
+window.addEventListener("scroll", event => {
+	audrey.style.width = window.scrollY + "px";
+	scroll = window.scrollY;
+	let width = scroll / 3;
+	if (width < 50) {
+		width = 50;
+	}
+	audrey.style.width = width + "px";
+	let height = scroll / 4;
+	if (height < 100) {
+		height = 100;
+	}
+	audrey.style.height = width + "px";
 });
+
+/*
+    Add an event listener to the `document` object to listen
+    for the "scroll" event.
+*/
+
+/*
+        Adjust the width of audrey to be 1/3 the value of
+        `window.scrollY`. No lower than 50px, though.
+    */
+/*
+        Adjust the height of audrey to be 1/4 the value of
+        `window.scrollY`. No lower than 100px, though.
+    */
