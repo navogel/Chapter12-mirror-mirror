285b3975d20fb8582384a24caa47857a13557a62
Nate V <navogel@gmail.com>
2019-09-03 17:44:06 -0500

    refactored scrolling and scaling

--- js/main.js
+++ js/main.js
@@ -3,31 +3,28 @@ console.log("hello");
 const audrey = document.getElementById("audrey");
 let scroll = window.scrollY;
 
+// window.addEventListener("scroll", event => {
+// 	audrey.style.width = window.scrollY + "px";
+// 	scroll = window.scrollY;
+// 	let width = scroll / 3;
+// 	if (width < 50) {
+// 		width = 50;
+// 	}
+// 	audrey.style.width = width + "px";
+// 	let height = scroll / 4;
+// 	if (height < 100) {
+// 		height = 100;
+// 	}
+// 	audrey.style.height = width + "px";
+// });
+
 window.addEventListener("scroll", event => {
-	audrey.style.width = window.scrollY + "px";
-	scroll = window.scrollY;
-	let width = scroll / 3;
-	if (width < 50) {
-		width = 50;
+	if (window.scrollY < 100) {
+		audrey.style.height = "100px";
+	} else if (window.scrollY < 50) {
+		audrey.style.width = "50px";
+	} else {
+		audrey.style.width = window.scrollY + "px";
+		audrey.style.height = window.scrollY + "px";
 	}
-	audrey.style.width = width + "px";
-	let height = scroll / 4;
-	if (height < 100) {
-		height = 100;
-	}
-	audrey.style.height = width + "px";
 });
-
-/*
-    Add an event listener to the `document` object to listen
-    for the "scroll" event.
-*/
-
-/*
-        Adjust the width of audrey to be 1/3 the value of
-        `window.scrollY`. No lower than 50px, though.
-    */
-/*
-        Adjust the height of audrey to be 1/4 the value of
-        `window.scrollY`. No lower than 100px, though.
-    */
